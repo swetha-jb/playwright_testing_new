a7b264b743e7ef856399ae4d22f7e2de281bf611
swetha <swetha.j@vvdntech.in>
2025-09-09 11:22:51 +0530

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from "@playwright/test";
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: "./tests",
-  use: {
-    browserName: "chromium",
-    headless: false,
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
-  },
-  reporter: [["list"], ["html", { outputFolder: "reports" }]],
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report/html' }],
+    ['junit', { outputFile: 'playwright-report/results.xml' }],
+  ],
 });
